f09cd48ede1fe370ddd9fb43b81972f3409cb401
Panos Sakkos <panos.sakkos@gmail.com>
2016-07-19 21:18:31 +0200

    v1.2.0
    - Fixes #48

--- src/Pods/Target Support Files/MBSwitch/MBSwitch-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_MBSwitch : NSObject
-@end
-@implementation PodsDummy_MBSwitch
-@end
--- src/Pods/Target Support Files/MBSwitch/MBSwitch-umbrella.h
+++
@@ -1,7 +0,0 @@
-#import <UIKit/UIKit.h>
-
-#import "MBSwitch.h"
-
-FOUNDATION_EXPORT double MBSwitchVersionNumber;
-FOUNDATION_EXPORT const unsigned char MBSwitchVersionString[];
-
